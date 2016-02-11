059a6737912929298502764fee36802c3e647ad5
sco-bo <scottebobbitt@gmail.com>
2016-02-10 22:56:35 -0500

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do 
+    get signup_path
+    assert_no_difference 'User.count' do 
+      post users_path, user: {
+        name: "", 
+        email: "scott@notvalid",
+        password: "foob",
+        password_confirmation: "boof"
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup info" do 
+    get signup_path
+    assert_difference 'User.count', 1 do 
+      post_via_redirect users_path, user: {
+        name: "Zach Weive", 
+        email: "zack@gmail.com",
+        password: "nunyabidness",
+        password_confirmation: "nunyabidness"
+      }
+    end
+    assert_template 'users/show'
+  end
+end
